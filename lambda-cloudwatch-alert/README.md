0138e0e1466df3a66fea4c3eec874611d1a59114
Renato Romao <renatolsromao@gmail.com>
2019-07-23 14:07:05 -0300

    INC sls framework configuration for cloudwatch+sns email alert on lambda errors;

---
+++ lambda-cloudwatch-alert/README.md
@@ -0,0 +1,5 @@
+# Lambda Cloudwatch Alert
+
+Create alerts for lambda executions error (or success..) using 
+CloudWatch and SNS (to send email), all configured with serverless
+framework.
--- lambda-cloudwatch-alert/handler.py
+++ lambda-cloudwatch-alert/handler.py
@@ -1,24 +1,14 @@
-import json
-
-
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    if 'raiseError' in event:
+        raise ValueError('Intentional throw error.')
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": ''
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+if __name__ == '__main__':
+    hello('', '')
